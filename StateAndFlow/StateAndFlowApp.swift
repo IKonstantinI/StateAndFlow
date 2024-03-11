34c50fc6b0901947c7fafe111c31a7994d2cab03
Konstantin <diel404@gmail.com>
2024-03-10 20:20:35 -0700

    Initial Commit

---
+++ StateAndFlow/StateAndFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndFlowApp.swift
+//  StateAndFlow
+//
+//  Created by horze on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct StateAndFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
